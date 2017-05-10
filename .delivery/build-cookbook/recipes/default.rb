cc6adde95dcee84af087409abec1c21352849091
K Suresh <Suresh.K02@mphasis.com>
2017-05-10 16:10:36 +0530

    New code for pet clinic

---
+++ .delivery/build-cookbook/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: build-cookbook
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# All applications that need to be installed on the build servers should go here
+
+
+
